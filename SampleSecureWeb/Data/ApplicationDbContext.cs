99a86005a099c04fb6ef765e5c1f4193feb2804b
yohanesdewantara <157205950+yohanesdewantara@users.noreply.github.com>
2024-10-02 15:39:03 +0700

    Project Secure Coding 1

---
+++ SampleSecureWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,20 @@
+using System;
+
+using SampleSecureWeb.Models;
+using Microsoft.EntityFrameworkCore;
+namespace SampleSecureWeb.Data
+{
+
+public class ApplicationDbContext : DbContext
+{
+   public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+    {
+
+    }
+
+    public DbSet<Student> Students {get; set;} = null!;
+    
+
+}
+}
---
+++ SampleSecureWeb/Data/IStudent.cs
@@ -0,0 +1,15 @@
+using System;
+using SampleSecureWeb.Models;
+
+namespace SampleSecureWeb.Data;
+
+public interface IStudent
+{
+    IEnumerable<Student> GetStudents();
+
+    Student GetStudent(string nim);
+    Student AddStudent(Student student);
+    Student UpdateStudent(Student student);
+    void DeleteStudent(string nim);
+
+}
